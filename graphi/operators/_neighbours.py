ea4ce1532fcc2491d2a8043ac8ba63594b3d4e09
Max Fischer <maxfischer2781@gmail.com>
2017-08-01 15:23:06 +0200

    added neighbours operator

---
+++ graphi/operators/_neighbours.py
@@ -0,0 +1,27 @@
+from . import interface
+
+
+@interface.graph_operator
+def neighbours(graph, node, maximum_distance=None):
+    """
+    Yield all nodes to which there is an outgoing edge from ``node`` in ``graph``
+
+    :param graph: graph in which to search for edges
+    :type graph: :py:class:`~graphi.abc.Graph`
+    :param node: node from which edges originate.
+    :param maximum_distance: maximum distance to other nodes
+    :return: iterator of neighbour nodes
+    :raises NodeError: if ``node`` is not in the graph
+
+    When ``maximum_distance`` is not :py:const:`None`, it is the maximum allowed edge value.
+    This is interpreted using the ``<=`` operator as ``graph[edge] <= distance``.
+
+    If there is a valid edge ``graph[node:node] <= distance``, then ``node``
+    is part of its own neighbourhood.
+
+    :note: The order of neighbours is arbitrary.
+    """
+    adjacency = graph[node]
+    if maximum_distance is None:
+        return iter(adjacency)
+    return (neighbour for neighbour in adjacency if adjacency[neighbour] <= maximum_distance)
---
+++ graphi_unittests/operators_unittests/test_neighbours.py
@@ -0,0 +1,50 @@
+import random
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from graphi.types import adjacency_graph
+from graphi.operators._neighbours import neighbours
+
+
+class TestNeighbours(unittest.TestCase):
+    @staticmethod
+    def _get_edge_values():
+        return [], [1, 2, 3, 4], [1, 2, 5, -3, 22, 42, -62, 21], [random.random() for _ in range(5)]
+
+    def test_operator(self):
+        """Operator neighbours(graph, node, ...)"""
+        for edges in self._get_edge_values():
+            with self.subTest(edges=edges):
+                graph = adjacency_graph.AdjacencyGraph(
+                    {'node': dict(enumerate(edges))}
+                )
+                # return all connected nodes - arbitrary order allowed
+                self.assertEqual(set(neighbours(graph, 'node')), set(range(len(edges))))
+                # return nodes up to a random element
+                for start_val in edges + [0, 1, 10, float('inf')]:
+                    self.assertEqual(
+                        set(neighbours(graph, 'node', start_val)),
+                        set(idx for idx, value in enumerate(edges) if value <= start_val)
+                    )
+                self.assertEqual(set(neighbours(graph, 'node', float('-inf'))), set())
+
+    def test_method(self):
+        """Overridden neighbours(graph, node, ...)"""
+        class NeighboursGraph(adjacency_graph.AdjacencyGraph):
+            def __graphi_neighbours__(self, node, maximum_distance=None):
+                if maximum_distance is None:
+                    return range(len(self[node]))
+                if maximum_distance < 0:
+                    return []
+                return range(int(maximum_distance))
+        for edges in self._get_edge_values():
+            with self.subTest(edges=edges):
+                graph = NeighboursGraph(
+                    {'node': dict(enumerate(edges))}
+                )
+                self.assertEqual(set(neighbours(graph, 'node')), set(range(len(edges))))
+                self.assertEqual(set(neighbours(graph, 'node', -1)), set())
+                self.assertEqual(set(neighbours(graph, 'node', 10)), set(range(10)))
