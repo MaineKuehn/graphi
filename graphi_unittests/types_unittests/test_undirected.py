a8f78198464f1bcc9d79bd7f922afc1ddb8db417
Eileen Kuehn <eileen.kuehn@kit.edu>
2018-07-27 14:59:57 +0200

    test for update of undirected graph with another undirected graph

--- graphi_unittests/types_unittests/test_undirected.py
+++ graphi_unittests/types_unittests/test_undirected.py
@@ -43,3 +43,8 @@ class TestUndirected(unittest.TestCase):
         self.assertIn(undirected.UndirectedEdge[1:3], undirected_graph)
         self.assertIn(undirected.UndirectedEdge[1:4], undirected_graph)
         self.assertNotIn(undirected.UndirectedEdge[2:3], undirected_graph)
+        second_graph = self.graph_cls({
+            2: {3: 1}
+        })
+        undirected_graph.update(second_graph)
+        self.assertIn(undirected.UndirectedEdge[2:3], undirected_graph)
