894e63d34d679b76af2e4ac057e842c9637e0480
Max Fischer <maxfischer2781@gmail.com>
2018-07-29 18:38:28 +0200

    added draft for bounded value graph wrapper

---
+++ graphi/types/bounded.py
@@ -0,0 +1,50 @@
+import collections as abc_collection
+
+from ..abc import Graph
+from .adjacency_graph import AdjacencyGraph
+
+
+class Bounded(Graph):
+    @property
+    def undirected(self):
+        return self._graph.undirected
+
+    def __init__(self, *source, **kwargs):
+        self.value_bound = kwargs.pop('value_bound')
+        self._graph = AdjacencyGraph()
+        super(Bounded, self).__init__(*source, **kwargs)
+        self._ensure_bounds()
+
+    # initialize a new graph by copying nodes, edges and values from another graph
+    def __init_graph__(self, graph, **kwargs):
+        # TODO: copy graph?
+        self._graph = graph
+        super(Bounded, self).__init_graph__(graph, **kwargs)
+
+    # initialize a new graph by copying nodes from an iterable
+    def __init_iterable__(self, iterable, **kwargs):
+        self._graph.update(iterable)
+        super(Bounded, self).__init_iterable__(iterable, **kwargs)
+
+    # initialize a new graph by copying nodes, edges and values from a nested mapping
+    def __init_mapping__(self, mapping, **kwargs):
+        self._graph.update(mapping)
+        super(Bounded, self).__init_mapping__(mapping, **kwargs)
+
+    def _ensure_bounds(self):
+        blacklist = []
+        for tail, head, value in self.items():
+            if value > self.value_bound:
+                blacklist.append((tail, head))
+        if self.undirected:
+            blacklist = {{tail, head} for tail, head in blacklist}
+        for tail, head in blacklist:
+            del self._graph[tail:head]
+
+    def __setitem__(self, item, value):
+        # do not add edges exceeding our maximum distance
+        if isinstance(item, slice) and value > self.value_bound:
+            return
+        elif isinstance(value, abc_collection.Mapping):
+            value = {node: value for node, value in value.items() if value <= self.value_bound}
+        super(Bounded, self).__setitem__(item, value)
