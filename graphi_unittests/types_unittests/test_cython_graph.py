bd586a7c94e64fa029def28897f249277a8dbd07
Max Fischer <maxfischer2781@gmail.com>
2018-08-07 17:58:25 +0200

    Cython Graph test properly runs on pypy

--- graphi_unittests/types_unittests/test_cython_graph.py
+++ graphi_unittests/types_unittests/test_cython_graph.py
@@ -12,5 +12,5 @@ from . import _graph_interface_mixins as mixins
 
 
 @unittest.skipIf(_graph is None, 'Cython extension not available')
-class TestAdjacencyGraphInterface(mixins.Mixin.GraphInitMixin, mixins.Mixin.GraphInterfaceMixin):
-    graph_cls = _graph.CythonGraph
+class TestCythonGraphInterface(mixins.Mixin.GraphInitMixin, mixins.Mixin.GraphInterfaceMixin):
+    graph_cls = _graph.CythonGraph if _graph is not None else None
