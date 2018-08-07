3b6980ae0166cdead921e89bf5009bb34cf9d0b2
Max Fischer <maxfischer2781@gmail.com>
2018-08-07 17:26:21 +0200

    added unittests for Cython graph

---
+++ graphi_unittests/types_unittests/test_cython_graph.py
@@ -0,0 +1,16 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+try:
+    from graphi.types import _graph
+except ImportError:
+    _graph = None
+
+from . import _graph_interface_mixins as mixins
+
+
+@unittest.skipIf(_graph is None, 'Cython extension not available')
+class TestAdjacencyGraphInterface(mixins.Mixin.GraphInitMixin, mixins.Mixin.GraphInterfaceMixin):
+    graph_cls = _graph.CythonGraph
